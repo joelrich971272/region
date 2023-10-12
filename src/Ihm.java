ddbaef8ef12c0e54e8db6ce93e93ac5e526dc08c
joelo <joelonrichardson2006@gmail.com>
2023-10-12 18:19:08 +0200

    case 4

--- src/Ihm.java
+++ src/Ihm.java
@@ -1,13 +1,17 @@
 import clavier.In;
 
+import java.util.zip.ZipEntry;
+
 public class Ihm {
     public static void main(String[] args) {
         String zoneGeographique = null;
-        String numeroDeTelephone = new String("0139757575");
         String zoneDep =null;
         int digitZoneGeo = 0 ;
         int digitDep = 0;
 
+
+        System.out.println("Veuillez saisir le numero de téléphone désiré ");
+        String numeroDeTelephone =In.readString();
         digitZoneGeo = Integer.parseInt(numeroDeTelephone.substring(1, 2));
         digitDep = Integer.parseInt(numeroDeTelephone.substring(2,4));
         switch (digitZoneGeo){
@@ -78,6 +82,8 @@ public class Ihm {
                         break;
                     case 22 :
                     case 30 :
+                    case 56 :
+                    case 90 :
                         zoneDep = new String("(22)Cotes-d'Armor/(29)Finistère/(35)Ille-et-Villaine/(56)Morbihan");
                         break;
                     case 28 :
@@ -85,6 +91,9 @@ public class Ihm {
                         zoneDep = new String("(44)Loire-Atlantique");
                         break;
                     case 32 :
+                    case 76 :
+                    case 77 :
+                    case 78 :
                         zoneDep = new String("(27)Eure/(76)Seine-Maritime");
                         break;
                     case 33 :
@@ -125,6 +134,18 @@ public class Ihm {
                     case 54 :
                         zoneDep = new String("(36)Indre/(41)Loir-et-Cher");
                         break;
+                    case 96 :
+                        zoneDep = new String("(22)Cotes-d'Armor");
+                        break;
+                    case 97 :
+                        zoneDep = new String("(56)Morbihan");
+                        break;
+                    case 98 :
+                        zoneDep = new String("(29)Finistère");
+                        break;
+                    case 99 :
+                        zoneDep = new String("(35)Ille-et-Vilaine");
+                        break;
 
 
                 }
@@ -138,16 +159,190 @@ public class Ihm {
                         break;
                     case 20 :
                         zoneDep = new String("(59)Nord");
+                        break;
+                    case 21 :
+                        zoneDep = new String("(62)Pas-de-Calais");
+                        break;
+                    case 22 :
+                        zoneDep = new String("(80)Somme");
+                        break;
+                    case 23 :
+                        zoneDep = new String("(02)Aisne");
+                        break;
+                    case 24 :
+                        zoneDep = new String("(08)Ardennes");
+                        break;
+                    case 25 :
+                        zoneDep = new String("(10)Aube/(52)Haute-Marne");
+                    case 26 :
+                        zoneDep = new String("(51)Marne");
+                        break;
+                    case 27 :
+                    case 28 :
+                        zoneDep = new String("(59)Nord");
+                        break;
+                    case 29 :
+                        zoneDep = new String("(54)Meurthe-et-Moselle/(88)Vosges");
+                        break;
+                    case 44 :
+                        zoneDep = new String("(60)Oise");
+                        break;
+                    case 45 :
+                    case 71:
+                        zoneDep = new String("(21)Cote-d'Or/(58)Nièvre/(71)Saone-et-Loire/(89)Yonne");
+                        break;
+                    case 51 :
+                    case 52 :
+                        zoneDep = new String("(08)Ardennes/(10)Aube/(51)Marne/(52)Guate-Marne");
+                        break;
+                    case 54 :
+                    case 55 :
+                    case 57 :
+                        zoneDep = new String("(54)Meurthe-et-Moselle/(55)Meuse/(57)Moselle/(88)Vosges");
+                        break;
+                    case 58 :
+                        zoneDep = new String("(21)Cote-d'Or/(58)Nièvre/(71)Saone-etLoire");
+                        break;
+                    case 60 :
+                    case 64:
+                    case 65:
+                        zoneDep = new String("(02)Aisne/(60)Oise/(80)Somme");
+                        break;
+                    case 61 :
+                    case 62 :
+                    case 66:
+                        zoneDep = new String("(59)Nord/(62)Pas-de-Calais");
+                        break;
+                    case 63 :
+                    case 70 :
+                        zoneDep = new String("(25)Doubs/(39)Jura/(70)Haute-Saone/(90)Territoire-de-Belfort");
+                        break;
+                    case 68:
+                    case 69:
+                        zoneDep = new String("(67)Bas-Rhin/(68)Haut-Rhin");
+                        break;
+                    case 80:
+                        zoneDep = new String("(21) Cote-d'Or");
+                        break;
+                    case 81:
+                        zoneDep = new String("(25)Doubs");
+                        break;
+                    case 82:
+                        zoneDep = new String("(54)Meurthe-et-Moselle/(57)Moselle");
+                        break;
+                    case 83:
+                        zoneDep = new String("(54)Meurthe-et-Moselle");
+                        break;
+                    case 84:
+                        zoneDep = new String("(39)Jura/(70)Haute-Saone/(90)Territoire-de-Belfort");
+                        break;
+                    case 85:
+                        zoneDep = new String("(71)Saone-et-Loire");
+                        break;
+                    case 86:
+                        zoneDep = new String("(58)Nièvre/(89)Yonne");
+                        break;
+                    case 87:
+                        zoneDep = new String("(57)Moselle");
+                        break;
+                    case 88:
+                    case 90:
+                        zoneDep = new String("(67)Bas-Rhin");
+                        break;
+                    case 89:
+                        zoneDep = new String("(68)Haut-Rhin");
+                        break;
+
+
                 }
                 break;
             case 4 :
                 zoneGeographique = new String("Region Sud Est");
                 switch (digitDep){
+                    case 11 :
+                    case 30 :
+                    case 34 :
+                        zoneDep = new String("(11)Aude/(30)Gard/(34)Hérault/(48)Lozère/(66)Pyrénées-Orientales");
+                        break;
+                    case 13 :
+                        zoneDep = new String("(04)Alpes-de-Haute-Provence/(05)Hautes-Alpes(13)Bouches-du-Rhone");
+                        break;
+                    case 15 :
+                    case 43 :
+                    case 63 :
+                        zoneDep = new String("(03)Allier/(15)Cantal/(43)Haute-Loire/(63)Puy-en-Dome");
+                        break;
                     case 20 :
                         zoneDep = new String("(2A)Corse-du-Sud");
                         break;
+                    case 26 :
+                    case 27 :
+                    case 81 :
+                        zoneDep = new String("(01)Ain/(07)Ardèche/(26)Drome/(42)Loire/(69)Rhone");
+                        break;
+
                     case 42 :
                         zoneDep = new String("(13)Bouche-du-Rhone");
+                        break;
+                    case 50 :
+                        zoneDep = new String("(01)Ain/(74)Haute-Savoie");
+                        break;
+                    case 56 :
+                    case 57 :
+                    case 80 :
+                        zoneDep = new String("(38)Isère(73)Savoie(74)Haute-Savoie");
+                        break;
+                    case 66 :
+                        zoneDep = new String("(30)Gard/(48)Lozère");
+                        break;
+                    case 67 :
+                        zoneDep = new String("(34)Hérault");
+                        break;
+                    case (70) :
+                        zoneDep = new String("(03)Allier");
+                        break;
+                    case 71 :
+                        zoneDep = new String("(15)Cantal/(43)Haute-Loire");
+                        break;
+                    case 72 :
+                    case 78 :
+                        zoneDep = new String("(69)Rhone");
+                        break;
+                    case 73 :
+                        zoneDep = new String("(63)Puy-en-Dome");
+                        break;
+                    case 74 :
+                        zoneDep = new String("(01)Ain");
+                        break;
+                    case 75 :
+                        zoneDep = new String("(07)Ardèche/(26)Drome");
+                        break;
+                    case 76 :
+                        zoneDep = new String("(38)Isère");
+                        break;
+                    case 79 :
+                        zoneDep = new String("(73)Savoie");
+                        break;
+                    case 83 :
+                    case 89 :
+                        zoneDep = new String("(06)Alpes-Maritimes/(83)Var");
+                        break;
+                    case 86 :
+                    case 88 :
+                        zoneDep = new String("(04)Alpes-de-Hautes-Provence/(05)Hauts-Alpes/(13)Bouches-du-Rhone");
+                        break;
+                    case 90 :
+                        zoneDep = new String("(13)Bouches-du-Rhone/(84)Vaucluse");
+                        break;
+                    case 91 :
+                        zoneDep = new String("(13)Bouches-du-Rhone");
+                        break;
+                    case 92 :
+                        zoneDep = new String("(04)Alpes-de-Hautes-Provence(05)Hauts-Alpes(83)Var");
+                        break;
+                    case 93 :
+                    case 97 :
+                        zoneDep = new String("(06)Alpes-Maritimes");
                         break;
                 }
                 break;
