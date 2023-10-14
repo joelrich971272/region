f6bd9fc9cf46f787ab91152dbf6037cb15ee89b7
joelo <joelonrichardson2006@gmail.com>
2023-10-14 21:29:51 +0200

    case 6

--- src/Ihm.java
+++ src/Ihm.java
@@ -349,17 +349,166 @@ public class Ihm {
             case 5 :
                 zoneGeographique = new String("Region Sud Ouest");
                 switch (digitDep){
+                    case 16 :
+                    case 17 :
+                        zoneDep = new String("(16)Charente/(17)Charente-Maritime/(79)Deux-Sèvres/(86)Vienne");
+                        break;
+                    case 19 :
+                        zoneDep = new String("(19)Corrèze/(23)Creuse/(87)Haute-Vienne");
+                        break;
+                    case 24 :
+                    case 33 :
+                    case 40 :
+                    case 47 :
+                        zoneDep = new String("(24)Dordogne/(33)Gironde/(40)Landes/(47)Lot-et-Garonne/(64)Pyrénées-Atlantiques");
+                        break;
+                    case 31 :
+                    case 67 :
+                    case 81 :
+                    case 82 :
+                        zoneDep = new String("(09)Ariège/(12)Aveyron/(31)Haute-Garonne/(32)Gers/(46)Lot/(65)Hautes-Pyrénées/(81)Tarn/(82)Tarn-et-Garonne");
+                        break;
                     case 34 :
                         zoneDep = new String("(31)Haute-Garonne");
                         break;
                     case 45 :
                         zoneDep = new String("(16)Charente");
                         break;
+                    case 46 :
+                        zoneDep = new String("(17)Charente-Maritime");
+                        break;
+                    case 49 :
+                        zoneDep = new String("(79)Deuc-Sèvres/(86)Vienne");
+                        break;
+                    case 53 :
+                        zoneDep = new String("(24)Dordogne/(47)Lot-et-Garonne");
+                        break;
+                    case 55 :
+                        zoneDep = new String("(19)Corrèze/(23)Creuse");
+                        break;
+                    case 56 :
+                    case 57 :
+                        zoneDep = new String("(33)Gironde");
+                        break;
+                    case 59 :
+                        zoneDep = new String("(64)Pyrénées-Atlantiques");
+                        break;
+                    case 61 :
+                        zoneDep = new String("(09)Ariège/(31)Haute-Garonne/(32)Gers/(65)Hautes-Pyrénées");
+                        break;
+                    case 62 :
+                        zoneDep = new String("(31)Haute-Garonne/(32)Gers/(65)Haute-Pyrénées");
+                        break;
+                    case 63 :
+                        zoneDep = new String("(81)Tarn/(82)Tarn-etGaronne");
+                        break;
+                    case 65 :
+                        zoneDep = new String("(12)Aveyron/(46)Lot");
+                        break;
+                    case 79 :
+                        zoneDep = new String("(16)Charente/(17)Charente-Maritime/(79)Deux-Sèvres/(86)Vienne");
+                        break;
+                    case 87 :
+                        zoneDep = new String("(19)Corrèze/(23)Creuse/(87)Haute-Vienne");
+                        break;
+                    case 90 :
+                        zoneDep = new String("(971)Guadeloupe");
+                        break;
+                    case 96 :
+                        zoneDep = new String("(972)Martinique");
+                        break;
+                    case 94 :
+                        zoneDep = new String("'973)Guyane");
+                        break;
                 }
                 break;
             case 6 :
                 zoneGeographique = new String("Operateur mobile");
                 switch (digitDep){
+                    case 01 :
+                        zoneDep = new String("Neuf Cegetel");
+                        break;
+                    case 3 :
+                    case 9 :
+                    case 10 :
+                    case 11 :
+                    case 12 :
+                    case 13 :
+                    case 14 :
+                    case 15 :
+                    case 16 :
+                    case 17 :
+                    case 18 :
+                    case 19 :
+                    case 20 :
+                    case 21 :
+                    case 22 :
+                    case 23 :
+                    case 24 :
+                    case 25 :
+                    case 26 :
+                    case 27 :
+                    case 28 :
+                    case 29 :
+                    case 35 :
+                    case 46 :
+                    case 55 :
+                        zoneDep = new String("SFR");
+                        break;
+                    case 05 :
+                        zoneDep = new String("Auchan Telecom");
+                        break;
+                    case 7 :
+                    case 8 :
+                    case 31 :
+                    case 32 :
+                    case 33 :
+                    case 34 :
+                    case 37 :
+                    case 42 :
+                    case 43 :
+                    case 45 :
+                    case 54 :
+                    case 70 :
+                    case 71 :
+                    case 72 :
+                    case 73 :
+                    case 74 :
+                    case 75 :
+                    case 76 :
+                    case 77 :
+                    case 78 :
+                    case 79 :
+                    case 80 :
+                    case 81 :
+                    case 82 :
+                    case 83 :
+                    case 84 :
+                    case 85 :
+                    case 86 :
+                    case 87 :
+                    case 88 :
+                    case 89 :
+                        zoneDep = new String("Orange");
+                        break;
+                    case 50 :
+                    case 53 :
+                    case 58 :
+                    case 59 :
+                    case 60 :
+                    case 61 :
+                    case 62 :
+                    case 63 :
+                    case 64 :
+                    case 65 :
+                    case 66 :
+                    case 67 :
+                    case 68 :
+                    case 69 :
+                    case 98 :
+                    case 99 :
+                        zoneDep = new String("Bouygues Telecom");
+                        break;
 
                 }
                 break;
